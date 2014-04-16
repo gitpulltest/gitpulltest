469911e31c3f900017620a809c281bd88e4d3f80
Yury_Burkouski <yury_burkouski@epam.com>
2014-04-16 18:25:12 +0300

    te3

--- mavenproject/src/main/java/com/mavenproject/App.java
+++ mavenproject/src/main/java/com/mavenproject/App.java
@@ -1,19 +1,29 @@
 package com.mavenproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class App {
 
     int a = 1;
+    static int b = 1;
     
     public static void main(final String[] args) {
         int i = 1;
         int j = 1;
         int k = 1;
         int n = 1;
+        b = b = 1;
         System.out.println( "Hello World!" );
         if ( true ){
             System.out.println();
         }
+        if (false){
+            return;
+        }
+        List l = new ArrayList();
+        l.removeAll(l);
     }
 
 }
