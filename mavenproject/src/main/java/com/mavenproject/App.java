3cb1750f84578bd9be803af3d94a88e41b9fd317
Yury_Burkouski <yury_burkouski@epam.com>
2014-04-16 21:09:18 +0300

    tt

--- mavenproject/src/main/java/com/mavenproject/App.java
+++ mavenproject/src/main/java/com/mavenproject/App.java
@@ -12,7 +12,7 @@ public class App {
     public static void main(final String[] args) {
         int i = 1;
         int j = 1;
-        b = b = 1;
+        b = b = 2;
         System.out.println( "Hello World!" );
         if ( true ){
             System.out.println();
